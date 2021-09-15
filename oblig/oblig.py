f95ac89280b7436344b73935c93093f96c2c8e44
Tage <tage.hisdal@gmail.com>
2021-09-15 15:02:56 +0200

    Started

---
+++ oblig/oblig.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+        self.prev = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+
+    
