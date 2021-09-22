eeab442f1f666a38b1158bf5ec15ab6ee7fdf7fb
Tage <tage.hisdal@gmail.com>
2021-09-22 13:42:15 +0200

    Add switch for input

--- oblig/main.py
+++ oblig/main.py
@@ -1,5 +1,50 @@
 from oblig import LinkedList
 
+showInfo()
+
+while True:
+    select = input()
+
+    switch(select):
+        case 1:
+            list.deleteFirst()
+            break
+        case 2:
+            value = input("What value would you like to add?")
+            list.addEndNode(value)
+            break
+        case 3:
+            list.deleteLast()
+            break
+        case 4:
+            value = input("What value would you like to delete?")
+            list.deleteSelectedValue(value)
+            break
+        case 5:
+            valueAdd = input("What value would you like to add")
+            valueWhere = input("Wich number do you want the value after?")
+            liste.addItemAfterValue(valueWhere, valueAdd)
+            break
+        case 6:
+            valueAdd = input("What value would you like to add")
+            valueWhere = input("Wich number do you want the value before?")
+            list.addItemBeforeValue(valueWhere, valueAdd)
+            break
+        case 7:
+            list.printLength()
+            break
+        case 8:
+            liste.countNumberOfOccurrences()
+            break
+        case 9:
+            list.print()
+            break
+        case 10:
+            list.deleteList()
+            break
+        default:
+            print("Enter a valid value")
+
 
 def showInfo():
     print("1: Delete an item first in the list")
