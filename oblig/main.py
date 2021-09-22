9da0552bdd7adf6142586175410637b343161e33
Tage <tage.hisdal@gmail.com>
2021-09-22 13:28:48 +0200

    Add funciton to show meny

--- oblig/main.py
+++ oblig/main.py
@@ -1,2 +1,14 @@
 from oblig import LinkedList
 
+
+def showInfo():
+    print("1: Delete an item first in the list")
+    print("2: Add an item to the end of the list")
+    print("3: Delete an item at the end of the list")
+    print("4: Delete an item with the specified value from the list")
+    print("5: Add an item after an item with the specified value")
+    print("6: Add an item in front of an item with the specified value")
+    print("7: Print the length of the list")
+    print("8: Count the number of occurrences of element with given value in the list, this number is printed")
+    print("9: Print the entire list")
+    print("10: Delete the entire list. How many items were deleted is printed")
