04e4eba4e6fdb35229b93d173d5c934612566247
Martin Wiulsrød <wiu753@gmail.com>
2021-09-22 09:23:56 +0200

    Update print function

--- oblig/oblig.py
+++ oblig/oblig.py
@@ -19,6 +19,7 @@ class LinkedList:
     #Task 2 Add one node to the end of the list
     def addEnd(self, data):
         newNode = Node(data)
+        
         if self.head is None: # If there are no nodes in the list make head and tail point to newly created node
             self.head = newNode
             self.tail = newNode
@@ -32,8 +33,8 @@ class LinkedList:
 
     #Task 3 - Delete the last node
     def deleteLast(self):
-        self.tail = self.tail.prev
-        self.tail.next = None
+        self.tail = self.tail.prev # Set the tail to the previous node
+        self.tail.next = None # Set the previous node's next pointer to null
 
     #Task 4 - Delete all nodes with specified value
     def deleteSelectedValue(self, data):
@@ -57,15 +58,24 @@ class LinkedList:
 
 
     #Task 9 - Print entire list
-    def print(self):
+    def print(self, backwards = False):
+        # Check if there is a node in the list
         current = self.head
         if current is None:
             print("Empty list")
             return
+        
+        if backwards is True: # If backwards is set to True print the list backwards
+            current = self.tail
+            while current is not None:
+                print(current.data)
+                current = current.prev
+        else: 
+            while current is not None:
+                print(current.data)
+                current = current.next
 
-        while current is not None:
-            print(current.data)
-            current = current.next
+        
             
 
 liste = LinkedList()
@@ -79,7 +89,7 @@ liste.addEnd(6)
 liste.addEnd(7)
 
 # liste.deleteSelectedValue(2)
-#liste.print()
+liste.print(True)
 
 
 
