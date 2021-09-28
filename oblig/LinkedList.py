f10831fedbd3ce64cae14699c90691700503d2b2
Martin Wiulsrød <wiu753@gmail.com>
2021-09-28 11:39:32 +0200

    Change naming of methods and remove all print functions

--- oblig/LinkedList.py
+++ oblig/LinkedList.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-from Node import Node
 class LinkedList:
     def __init__(self, Node):
         self.head, self.tail = None, None
@@ -21,17 +20,17 @@ class LinkedList:
         ]
 
 
-    def empty_list(self, node):
+    def node_exists(self, node):
         if node is None:
-            print("List is empty")
-            return True
-        return False
+            return False
+        return True
 
     def delete_first_node(self):
         self.head = self.head.next
         return
-
+    
     def add_end_node(self, data):
+    def add_node_to_end(self, data):
         newNode = self.node(data)
         
         # If there are no nodes
@@ -46,7 +45,7 @@ class LinkedList:
             self.tail.next = newNode 
             self.tail = newNode
         return
-
+    
     def delete_last_node(self):
         self.tail = self.tail.prev
         self.tail.next = None
@@ -55,139 +54,135 @@ class LinkedList:
     def delete_nodes_with_value(self, value):
         curr = self.head
 
-        if self.emptyList(curr):
-            return
+        if self.node_exists(curr):
+            while curr is not None:
+                if value == curr.data:
+                    # If previous node doesn't exist, move head
+                    if curr.prev is None:
+                        self.head = curr.next
+                    
+                    # If next node doesn't exist, move tail
+                    if curr.next is None:
+                        self.tail = curr.prev
 
-        while curr is not None:
-            if value == curr.data:
-                # If previous node doesn't exist, move head
-                if curr.prev is None:
-                    self.head = curr.next
-                
-                # If next node doesn't exist, move tail
-                if curr.next is None:
-                    self.tail = curr.prev
-
-                if curr.prev is not None:
-                    tmp = curr.prev
-                    tmp.next = curr.next
-                
-                if curr.next is not None:
-                    tmp = curr.next
-                    tmp.prev = curr.prev
-            curr = curr.next.next
-        return
+                    if curr.prev is not None:
+                        tmp = curr.prev
+                        tmp.next = curr.next
+                    
+                    if curr.next is not None:
+                        tmp = curr.next
+                        tmp.prev = curr.prev
+                curr = curr.next.next
+            return True
+        return False
 
     def add_node_after_value(self, val, insVal):
         curr = self.head
 
-        if self.emptyList(curr):
-            return
-        
-        while curr is not None:
-            newNode = self.node(insVal)
-            if curr.data == val:
-                if curr.next is not None:
-                    tmp = curr.next
-                    newNode.next = tmp
-                    curr.next = newNode
-                    newNode.prev = curr
-                    tmp.prev = newNode
-                    curr = curr.next.next
-                    continue
-                
-                # If on last node, update tail
-                if curr.next is None:
-                    self.tail = newNode
-                    curr.next = newNode
-                    newNode.prev = curr
-                    break
-            curr = curr.next
-        return   
+        if self.node_exists(curr):
+            while curr is not None:
+                newNode = self.node(insVal)
+                if curr.data == val:
+                    if curr.next is not None:
+                        tmp = curr.next
+                        newNode.next = tmp
+                        curr.next = newNode
+                        newNode.prev = curr
+                        tmp.prev = newNode
+                        curr = curr.next.next
+                        continue
+                    
+                    # If on last node, update tail
+                    if curr.next is None:
+                        self.tail = newNode
+                        curr.next = newNode
+                        newNode.prev = curr
+                        break
+                curr = curr.next
+                return True
+        return False
 
     def add_node_before_value(self, val, insVal):
         curr = self.tail
 
-        if self.emptyList(curr):
-            return
-        
-        while curr is not None:
-            newNode = self.node(insVal)
-            if curr.data == val:
-                if curr.prev is not None:
-                    tmp = curr.prev
-                    newNode.prev = tmp
-                    curr.prev = newNode
-                    newNode.next = curr
-                    tmp.next = newNode
-                    curr = curr.prev.prev
-                    continue
-                
-                # If on first node, update head
-                if curr.prev is None:
-                    self.head = newNode
-                    curr.prev = newNode
-                    newNode.next = curr
-                    break
-            curr = curr.prev
-        return   
+        if self.node_exists(curr):
+            while curr is not None:
+                newNode = self.node(insVal)
+                if curr.data == val:
+                    if curr.prev is not None:
+                        tmp = curr.prev
+                        newNode.prev = tmp
+                        curr.prev = newNode
+                        newNode.next = curr
+                        tmp.next = newNode
+                        curr = curr.prev.prev
+                        continue
+                    
+                    # If on first node, update head
+                    if curr.prev is None:
+                        self.head = newNode
+                        curr.prev = newNode
+                        newNode.next = curr
+                        break
+                curr = curr.prev
+            return True
+        return False
 
     
     def list_length(self):
+    def get_list_length(self):
         curr = self.head
         count = 0
 
-        if self.emptyList(curr):
-            return
-
-        while curr is not None:
-            curr = curr.next
-            count += 1
-        return count
+        if self.node_exists(curr):
+            while curr is not None:
+                curr = curr.next
+                count += 1
+            return count
+        return False
 
     def count_occourences(self, value):
+    def get_occourences(self, value):
         curr = self.head
         count = 0
         
-        if self.emptyList(curr):
-            return
-        
-        while curr is not None:
-            if curr.data == value:
-                count += 1
+        if self.node_exists(curr):
+            while curr is not None:
+                if curr.data == value:
+                    count += 1
 
-            curr = curr.next
-        return count
+                curr = curr.next
+            return count
+        return False
 
 
     def print_list(self, backwards = False):
+    def get_list(self, backwards = False):
         curr = self.head
+        list = []
         
-        if self.emptyList(curr):
-            return
-        
-        if backwards is True:
-            curr = self.tail
-            while curr is not None:
-                print(curr.data)
-                curr = curr.prev
-        else: 
-            while curr is not None:
-                print(curr.data)
-                curr = curr.next
-        return
+        if self.node_exists(curr):
+            if backwards is True:
+                curr = self.tail
+                while curr is not None:
+                    list.append(curr.data)
+                    curr = curr.prev
+            else: 
+                while curr is not None:
+                    list.append(curr.data)
+                    curr = curr.next
+            return list
+        return False
 
-                
     def delete_list(self):
         curr = self.head
         count = 0
 
-        if self.emptyList(curr):
-            return
-
-        while curr is not None:
-            self.head = None
-            self.tail = None
-            curr = curr.next
-            count += 1
-        return count
+        if self.node_exists(curr):
+            while curr is not None:
+                self.head = None
+                self.tail = None
+                curr = curr.next
+                count += 1
+            return count
+        return False
